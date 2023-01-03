674e789614f37097e3f752801bfee2d6e064bb03
Jacob Hillock <jacobh@emprise1.com>
2023-01-03 14:55:50 -0500

    small refactor for separator and file_path

--- src/file_utils.py
+++ src/file_utils.py
@@ -4,17 +4,17 @@ from os.path import exists
 
 class FS:
     def __init__(self, relative_file: str):
-        self.separator = '\\' if FS.is_windows else '/'
-        self.directory = relative_file.split(self.separator)[:-1]
+        self.directory = relative_file.split(FS.separator())[:-1]
+
+    def separator() -> str:
+        return '\\' if FS.is_windows else '/'
 
     @property
     def is_windows() -> bool:
         return os_name == 'nt'
 
     def get_file_path(self, file_name: str) -> str:
-        base_directory = self.separator.join(self.directory)
-        file_path = f"{base_directory}{self.separator}{file_name}"
-        return file_path
+        return FS.separator().join(self.directory + [file_name])
 
     def get_file(self, file_name: str) -> str:
         file_path = self.get_file_path(file_name)
